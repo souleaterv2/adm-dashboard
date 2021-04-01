8049ec92ff86a9b22f70c8dae045465e1bc7835a
Rodrigo Silva <rodsilvavieira@gmail.com>
2021-04-01 13:20:09 -0400

    component-header

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Header";
+import Head from "next/head";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Head>
+        <title> Dashboard | dashgo </title>
+      </Head>
+      <Header />
+    </>
+  );
+}
